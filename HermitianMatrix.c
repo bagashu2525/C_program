05ee7f78af1a3c6828b077a56cda4d17e7d134c7
bagashu2525 <bagasmita25@gmal.com>
2024-05-01 12:06:29 +0530

    Hermitian Matrix

---
+++ HermitianMatrix.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int real;
+  int imag;
+} Complex;
+
+typedef struct {
+  int *row;
+  int *col;
+  Complex *val;
+  int size;
+} SparseMatrix;
+
+
+int main(){
+    
+    return 0;
+}
